e496770a718329426e5e30632f2e27258e841265
@itzzfalcon52 <hussaintaherkagalwala5253@gmail.com>
2025-09-10 17:58:19 +0530

    added keypress functionalities

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,28 @@
 //import { useEffect, useState } from "react";
 
+import { useEffect, useRef } from "react";
 import { UseWeather } from "../contexts/WeatherContext";
 
 function Search() {
   const { city, setCity, error, onClickSearch } = UseWeather();
+  const inputEl = useRef(null);
+  useEffect(
+    function () {
+      function callBack(e) {
+        if (document.activeElement === inputEl.current) return; //we use this as after searching,we press enter then it will remove the movies list and go back to initial stae
+        if (e.key === " " || e.code === "Space") {
+          e.preventDefault();
+          inputEl.current.focus();
+          setCity("");
+        }
+        document.addEventListener("keydown", callBack);
+        return () => document.removeEventListener("keydown", callBack);
+      }
+      document.addEventListener("keydown", callBack);
+      return () => document.removeEventListener("keydown", callBack);
+    },
+    [setCity]
+  );
   return (
     <div className="search mt-12 flex justify-center items-center max-sm:flex-col max-sm:justify-between max-sm:mx-2  ">
       <div className="relative basis-128 max-sm:basis-0 max-sm:mb-2 max-sm:w-full ">
@@ -21,6 +40,13 @@ function Search() {
           value={city}
           onChange={(e) => {
             setCity(e.target.value);
+          }}
+          ref={inputEl}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              e.preventDefault(); // stop form submit/reload
+              onClickSearch(); // trigger search
+            }
           }}
         />
       </div>
