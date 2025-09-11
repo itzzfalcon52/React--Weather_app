db970197040d0e7ca2817af711c1f3e1473b4889
@itzzfalcon52 <hussaintaherkagalwala5253@gmail.com>
2025-09-11 13:32:54 +0530

    fixed border and margin issues in UI

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -9,7 +9,7 @@ function Result({ city, code, setShow, setCity }) {
   }
   return (
     <li
-      className="hover:bg-slate-800 flex-1 h-full text-white font-bricolage text-lg font-semibold m-2 flex items-center pl-2"
+      className="hover:bg-slate-800 flex-1 h-full text-white font-bricolage text-lg font-semibold m-2 flex items-center pl-2 cursor-pointer"
       onClick={handleClick}
     >
       {city},{code}
